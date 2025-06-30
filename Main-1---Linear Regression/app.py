639160715b0e836b640d0ff09f687c712ca576f7
surajprajapat0764 <surajprajapat0764@gmail.com>
2025-06-30 12:02:21 +0530

    machine

--- Main-1---Linear Regression/app.py
+++ Main-1---Linear Regression/app.py
@@ -1,15 +1,65 @@
 from flask import Flask,render_template,request,url_for
+
+import joblib,pickle
+import pandas as pd 
+
+model = pickle.load(open('model.pkl','rb'))
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('home.html')
 
-@app.route('/contact')
+@app.route('/Project')
 def contact():
-   return render_template('contact.html')
+   return render_template('Projects.html')
 
+@app.route("/predict",methods=["POST"])
+def predict():
+   if request.method=="POST" :
+      try:
+         brand_name = request.form['brand_name']
+         owner_name = int(request.form['owner'])
+         age_bike   = int(request.form['age'])
+         power_bike = int(request.form['power'])
+         kms_driven_bike = int(request.form['kms_driven'])
+
+
+         bike_numbers ={'TVS': 0,
+                                      'Royal Enfield': 1,
+                                      'Triumph': 2,
+                                      'Yamaha': 3,
+                                      'Honda': 4,
+                                      'Hero': 5,
+                                      'Bajaj': 6,
+                                      'Suzuki': 7,
+                                      'Benelli': 8,
+                                      'KTM': 9,
+                                      'Mahindra': 10,
+                                      'Kawasaki': 11,
+                                      'Ducati': 12,
+                                      'Hyosung': 13,
+                                      'Harley-Davidson': 14,
+                                      'Jawa': 15,
+                                      'BMW': 16,
+                                      'Indian': 17,
+                                      'Rajdoot': 18,
+                                      'LML': 19,
+                                      'Yezdi': 20,
+                                      'MV': 21,
+                                     'Ideal': 22}
+         brand_name_encoded = bike_numbers.get(brand_name)
+         input_data = [[owner_name,
+                        brand_name_encoded,
+                        kms_driven_bike,
+                          age_bike,power_bike]]
+         prediction = model.predict(input_data)
+         prediction = round(prediction[0],2)
+         return render_template('Projects.html',prediction=prediction)
+      except:
+         return 'something is wrong'
 
 
 if __name__=='__main__':
    app.run(debug=True)
+
